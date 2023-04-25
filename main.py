cfb70d359a493131662b50fc25148a0bb3a9ad0d
RossohinPavel <rossohinpavel@yandex.ru>
2023-04-25 07:55:47 +0300

    Визуал upd

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ listbox_vars = []
 
 
 def show_result_frame():
-    result_listbox.pack(expand=1, fill=tk.X)
+    result_listbox.pack(expand=1, fill=tk.BOTH)
     minus_button = tk.Button(root, text='–', width=4, font='arial 12 bold')
     minus_button.pack(anchor=tk.E)
 
@@ -17,9 +17,9 @@ def show_work_frame(lbl, side):
     frame = tk.Frame(root)
     label = tk.Label(frame, text=lbl)
     label.pack(expand=1, fill=tk.X)
-    list_box = tk.Listbox(frame)
+    list_box = tk.Listbox(frame, height=20)
     listbox_vars.append(list_box)
-    list_box.pack(expand=1, fill=tk.X)
+    list_box.pack(expand=1, fill=tk.BOTH)
     button_frame = tk.Frame(frame)
     button_frame.pack(expand=1, fill=tk.X)
     plus_button = tk.Button(button_frame, text='+', width=4, font='arial 12 bold', command=None)
