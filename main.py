928414e6ccfb33c640f25aa2ff73bc08c0b0d382
RossohinPavel <rossohinpavel@yandex.ru>
2023-04-25 07:49:52 +0300

    Визуал

--- main.py
+++ main.py
@@ -3,24 +3,34 @@ import tkinter as tk
 
 root = tk.Tk()
 
-result_list = ['test', 'test1']
-result_list_var = tk.Variable(root, value=result_list)
-
-
-def del_result_value():
-    result_list.pop()
-    root.update()
+result_listbox = tk.Listbox(root)
+listbox_vars = []
 
 
 def show_result_frame():
-    listbox = tk.Listbox(root, listvariable=result_list_var)
-    listbox.pack(expand=1, fill=tk.X)
+    result_listbox.pack(expand=1, fill=tk.X)
+    minus_button = tk.Button(root, text='–', width=4, font='arial 12 bold')
+    minus_button.pack(anchor=tk.E)
+
+
+def show_work_frame(lbl, side):
     frame = tk.Frame(root)
-    minus_button = tk.Button(root, text='-', command=del_result_value)
-    minus_button.pack(side=tk.RIGHT)
-    frame.pack(expand=1, fill=tk.X)
+    label = tk.Label(frame, text=lbl)
+    label.pack(expand=1, fill=tk.X)
+    list_box = tk.Listbox(frame)
+    listbox_vars.append(list_box)
+    list_box.pack(expand=1, fill=tk.X)
+    button_frame = tk.Frame(frame)
+    button_frame.pack(expand=1, fill=tk.X)
+    plus_button = tk.Button(button_frame, text='+', width=4, font='arial 12 bold', command=None)
+    plus_button.pack(side=tk.LEFT, expand=1, fill=tk.X)
+    minus_button = tk.Button(button_frame, text='–', width=4, font='arial 12 bold', command=None)
+    minus_button.pack(side=tk.RIGHT, expand=1, fill=tk.X)
+    frame.pack(side=side, expand=1, fill=tk.X)
 
 
 if __name__ == '__main__':
     show_result_frame()
+    show_work_frame('Обложки', tk.LEFT)
+    show_work_frame('Развороты', tk.RIGHT)
     root.mainloop()
